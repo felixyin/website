d8fa83c8bd7b0b7b64f38692ae7c6bc2c9f0ecb5
felixyin <ybkk1027@gmail.com>
2018-12-14 12:04:35 +0800

    青岛前途软件技术有限公司

--- update.sh
+++ update.sh
@@ -50,7 +50,7 @@ python manage.py migrate
 # 停止和启动memcached缓存服务
 systemctl restart memcached
 systemctl enable memcached
-systemctl status memcached
+#systemctl status memcached
 
 
 # 停止和启动web服务集群
@@ -59,12 +59,13 @@ nohup uwsgi --ini uwsgi.ini & ls
 
 # 停止和启动nginx
 systemctl restart nginx
-systemctl status nginx
+#systemctl status nginx
 
 
 # 产看启动的集群进程，好放心
-ps -ef | grep uwsgi
+#ps -ef | grep uwsgi
 
 
 # 回到之前的目录
 cd -
+ls
